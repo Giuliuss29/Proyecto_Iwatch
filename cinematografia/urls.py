51961f15bb33f2cd9bf5f2b96c3d2e7ebbaedbe0
Giuliuss29 <giuliacosta29@hotmail.com>
2024-07-02 17:08:49 -0300

    v2.01
    Se avanzo

--- cinematografia/urls.py
+++ cinematografia/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
 from cinematografia import views
-from .views import agregar_peli, agregar_series, inicio
+from .views import agregar_peli, agregar_series, about_me, prueba, detalle_pelicula, detalle_serie
 
 urlpatterns = [
-    path('',inicio, name='inicio'),
-    path('prueba/', views.Prueba.as_view(), name='prueba'),
-    path('peliculas/', views.Peliculas.as_view(), name='peliculas'),
-    path('series/', views.Series.as_view(), name='series'),
+    
+    path('', prueba, name='prueba'),
+    path('pelicula/<int:pk>',detalle_pelicula,name='detalle_pelicula'),
+    path('serie/<int:pk>',detalle_serie,name='detalle_serie'),
+    path('peliculas/', views.Peliculas01.as_view(), name='peliculas'),
+    path('series/', views.Series01.as_view(), name='series'),
     path('agregar_peli/', agregar_peli,name='agregar_peli'),
     path('agregar_series/', agregar_series,name='agregar_series'),
+    path('about_me/', about_me,name= 'about_me')
     
     
     
--- cinematografia/views.py
+++ cinematografia/views.py
@@ -1,30 +1,47 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView
 from .models import Peliculas, Series
 from .forms import FormPelicula, FormSeries
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 
-class Peliculas(ListView):
+class Peliculas01(ListView):
     model = Peliculas
     template_name = 'peliculas.html'
     
     
 
-class Series(ListView):
+class Series01(ListView):
     model = Series
     template_name = 'series.html'
     
 
 
-def inicio(request):
-    return render(request, 'inicio.html')
+def prueba(request):
+    peliculas = Peliculas.objects.all()
+    series = Series.objects.all()
+    items = list(peliculas)+list(series)
+    for item in items: 
+        item.is_pelicula = isinstance(item, Peliculas)
+    paginator = Paginator(items,1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj,
+    }
+    return render(request, 'prueba.html', context)
 
 
+def detalle_pelicula(request, pk):
+    pelicula = get_object_or_404(Peliculas, pk=pk) 
+    return render(request, 'articulo_pelicula.html', {'pelicula':pelicula})
 
-class Prueba(ListView):
-    model = Peliculas
-    template_name = 'prueba.html'
+
+def detalle_serie(request, pk):
+    serie = get_object_or_404(Series, pk=pk) 
+    return render(request, 'articulo_serie.html', {'serie':serie})
+
     
         
 @login_required(login_url='login')    
@@ -56,6 +73,11 @@ def agregar_series(request):
     else: 
         formulario=FormSeries()
     return render(request,'agregar_series.html',{'formulario':formulario})  
+
+
+
+def about_me(request):
+    return render(request, 'about_me.html')
 
 
 
